3909b077fe84893f689ca45c426809be05786257
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-01-08 16:52:27 +0530

    update server url

--- client/script.js
+++ client/script.js
@@ -62,7 +62,7 @@ const handleSubmit = async (e) => {
  
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch('https://a-t-a-i-s.onrender.com/', {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
